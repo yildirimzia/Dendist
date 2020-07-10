222458ab1aa5cf9eefeda9fdaa153fa5125bd61c
ziya yıldırım <ziya.yildirim@aknon.comm>
2020-07-10 12:18:48 +0300

    maps add

--- src/views/location.js
+++ src/views/location.js
@@ -1,12 +1,32 @@
 import * as React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+
 
 function AboutScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>konum</Text>
+    <View style={styles.container}>
+     <MapView
+      style={styles.maps}
+      initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+    />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    flex: 1,
+  }
+})
 
 export default AboutScreen;
