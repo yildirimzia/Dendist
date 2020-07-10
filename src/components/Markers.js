ab3458fea0cc71dbceaed8c37b1f765f27a84245
ziya yıldırım <ziya.yildirim@aknon.comm>
2020-07-11 00:09:03 +0300

    maps add

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,50 +1,32 @@
 import * as React from 'react';
-import { View, Text, StyleSheet,Image } from 'react-native';
+import { View, Text, StyleSheet, } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 
 function Markers() {
-  state={ 
-    region:{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.015,
-      longitudeDelta: 0.0121,
-	  },
-    markers: [
-        {
-          latlng: {
-            latitude: 37.78825,
-            longitude: -122.4324,
-          },
-          title: 'Diş Hekimi Tuğba Duymaz',
-          description: 'Feneryolu Mahallesi, Bağdat Cd. No:117/8, 34726 Kadıköy/İstanbul'
-        }
-    ]  
-  };
-  
 
   return (
     <View style={styles.container}>
-     <MapView
-       style={styles.map}
-       region={this.state.region}
-       loadingEnabled={true}
-        showsUserLocation={true}>
-
-       {
-         this.state.markers.map((marker,key) =>( 
-          <Marker 
-            key={key}
-            coordinate={marker.latlng}
-            title={marker.title}
-            description={marker.description}
-          >
-            <Image  source={require('../assets/location1.png')} />
-            </Marker>
-         ))
-       }
-     </MapView>
-    </View>
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    >
+       <Marker 
+        coordinate={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        }}
+        image={require('../assets/location1.png')}
+        title= "Diş Hekimi Tuğba Duymaz"
+        description= "Feneryolu Mahallesi, Bağdat Cd. No:117/8, 34726 Kadıköy/İstanbul"
+      >
+      </Marker>
+    </MapView>
+  </View>
   );
 }
 
