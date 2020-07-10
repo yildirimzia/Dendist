5d2d751b86a8cf84f0fe7592fb2e4ec4525010dd
ziya yıldırım <ziya.yildirim@aknon.comm>
2020-07-10 12:34:50 +0300

    google maps add two

--- src/views/location.js
+++ src/views/location.js
@@ -5,28 +5,33 @@ import MapView from 'react-native-maps';
 
 function AboutScreen() {
   return (
-    <View style={styles.container}>
+<View style={styles.container}>
      <MapView
-      style={styles.maps}
-      initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-    />
-    </View>
+      
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: '#F5FCFF',
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
-    flex: 1,
-  }
-})
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
 
 export default AboutScreen;
